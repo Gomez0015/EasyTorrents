770e2ac9c52bcbcc45fe511ca8c9561fc97936f7
Gomez0015 <oscargomez.raxo@gmail.com>
2021-08-21 21:21:40 +0200

    First Commit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const controller = require('../controllers/controller.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    controller.search(req, res);
+    if (req.headers.get('X-RapidAPI-Proxy-Secret') == "9be66f80-02b1-11ec-a11b-1f5a4b32332b") {
+        controller.search(req, res);
+    } else {
+        res.send({ error: "Not Authorized" });
+    }
 });
 
 module.exports = router;
